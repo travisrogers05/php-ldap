32f38efb3e887cc580f826f226637ed6d819e9d6
Travis Rogers <trogers@redhat.com>
2018-05-04 09:02:26 -0500

    Adding assemble script

---
+++ index.php
@@ -0,0 +1,14 @@
+
+<?php
+
+// Show all information, defaults to INFO_ALL
+//phpinfo();
+
+// Show just the module information.
+// phpinfo(8) yields identical results.
+//phpinfo(INFO_MODULES);
+
+var_dump($_ENV);
+
+?>
+
